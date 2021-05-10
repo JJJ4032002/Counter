533195343d688609595fcce4a52b44c79cbb7a9e
JJJ4032002 <rp4888888@gmail.com>
2021-05-10 10:42:26 +0530

    Last commit

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const inc = document.querySelector("#Inc");
 const dec = document.querySelector("#Dec");
 const res = document.querySelector("#Res");
 const count = document.querySelector("#count");
+const head = document.querySelector("#Head");
 let ct = 0;
 inc.addEventListener("click",()=>{
     ct++;
+     document.body.style.background = "#43b265";
+     count.style["color"] = "#06511d";
+     head.style["color"] = "white";
     console.log(ct);
     count.innerHTML = ct;
     
@@ -13,11 +17,20 @@ inc.addEventListener("click",()=>{
 dec.addEventListener("click",()=>{
     ct--;
     console.log(ct);
+    document.body.style.background = "#e5576c";
+    count.style["color"] = "#770416";
+    head.style["color"] = "white";
+    
     count.innerHTML = ct;
 });
 
 res.addEventListener("click",()=>{
     ct = 0;
     console.log(ct);
+     document.body.style.background = "white";
+     count.style["color"] = "black";
+      head.style["color"] = "black";
+      
+
     count.innerHTML = ct;
 })
