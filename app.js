592765f79f632d5c64814f99e499ce4c1c676863
JJJ4032002 <rp4888888@gmail.com>
2021-05-10 10:50:53 +0530

    javascript commit

--- app.js
+++ app.js
@@ -6,9 +6,23 @@ const head = document.querySelector("#Head");
 let ct = 0;
 inc.addEventListener("click",()=>{
     ct++;
-     document.body.style.background = "#43b265";
+    if(ct < 0){
+ document.body.style.background = "#e5576c";
+    count.style["color"] = "#770416";
+    head.style["color"] = "white";
+    }
+     else if(ct == 0){
+         document.body.style.background = "white";
+     count.style["color"] = "black";
+     head.style["color"] = "black";
+
+    }
+    else{
+         document.body.style.background = "#43b265";
      count.style["color"] = "#06511d";
      head.style["color"] = "white";
+    }
+      
     console.log(ct);
     count.innerHTML = ct;
     
@@ -17,9 +31,23 @@ inc.addEventListener("click",()=>{
 dec.addEventListener("click",()=>{
     ct--;
     console.log(ct);
-    document.body.style.background = "#e5576c";
+    if(ct < 0){
+ document.body.style.background = "#e5576c";
     count.style["color"] = "#770416";
     head.style["color"] = "white";
+    }
+    else if(ct == 0){
+         document.body.style.background = "white";
+     count.style["color"] = "black";
+     head.style["color"] = "black";
+    }
+    else{
+         document.body.style.background = "#43b265";
+     count.style["color"] = "#06511d";
+     head.style["color"] = "white";
+    }
+    
+    
     
     count.innerHTML = ct;
 });
